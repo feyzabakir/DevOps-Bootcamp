993f270ff79d30e93bcdb154c0ebd014b5c860be
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2024-03-24 20:14:01 +0300

    ...

--- db-app/helper/mysql.js
+++ db-app/helper/mysql.js
@@ -1,14 +1,12 @@
-
-require('dotenv').config(); 
-
 const mysql = require("mysql2");
 
 const connection = mysql.createPool({
   connectionLimit: 100,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: "mysql_db",
+  user: "user",
+  port: "3306",
+  password: "password",
+  database: "app",
 });
 
 module.exports = connection;
